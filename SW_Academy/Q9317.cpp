afd172c9d47817e999cb7e0399a6c737820a878a
pnm6054 <31690232+pnm6054@users.noreply.github.com>
2020-04-08 21:26:11 +0900

    Today's commit
    little bit updated

--- SW_Academy/Q9317.cpp
+++ SW_Academy/Q9317.cpp
@@ -7,14 +7,14 @@ int main(void)
 	int TC, N, ans;
 	string s1, s2;
 	cin >> TC;
-	for (int i = 0; i < TC; i++) {
+	for (int i = 1; i <= TC; i++) {
 		cin >> N >> s1 >> s2;
 		ans = 0;
 		for (int j = 0; j < N; j++) {
 			if (s1[j] == s2[j])
 				ans++;
 		}
-		cout << "#" << i + 1 << " " << ans << endl;
+		cout << "#" << i << " " << ans << endl;
 	}
 	return 0;
 }
